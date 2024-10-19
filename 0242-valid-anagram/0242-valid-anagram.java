bbcbbf044309ad103c2e4b3bf3d248e759815951
Morgan Bentley <126817281+Morgan-Bentley@users.noreply.github.com>
2024-10-19 14:25:37 +0200

    Time: 16 ms (23.71%), Space: 45.1 MB (5.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,17 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        int [] vals = new int[26];
-        
-        for(char a : s.toCharArray()) {
-            vals[a - 'a']+=1;
-        }
-        
-        for(char a : t.toCharArray()) {
-            vals[a - 'a']-=1;
-        }
-        
-        for(int a : vals) {
-            if(a != 0) {
-                return false;
-            }
-        }
-        return true;
+    public boolean isAnagram(String str1, String str2) {
+        if(str1.length() != str2.length()) return false;
+         HashMap<Character,Integer> map= new HashMap<>();
+         for(char c: str1.toCharArray()){
+             map.put(c,map.getOrDefault(c,0)+1);
+         }
+         for(char c: str2.toCharArray()){
+             if(map.getOrDefault(c,0) > 0){
+                 map.put(c,map.get(c)-1);
+             } else {
+                 return false;
+             }
+         }
+         return true;
     }
 }
